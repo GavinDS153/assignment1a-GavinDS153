3fa7bd0e40cd08e04df97888b9b10dfb8ff947cc
Gavin Sweeney <Gavinds153@gmail.com>
2024-09-11 18:02:08 -0400

    First commit

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "GavinDS153";
+    char STUDENT_ID[] = "966865813";
 
     return 0 ;
 }
